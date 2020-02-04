de6b9156ae707f82cb7676cfab199f1a3a533c12
Gaurangi <manegaurangi89@gmail.com>
2020-02-04 16:43:52 +0100

    Changes made as per review

--- Landing Page/js/app.js
+++ Landing Page/js/app.js
@@ -17,53 +17,57 @@
 const nav_bar=document.getElementsByClassName('navbar__menu');
 const nav_list=document.getElementById('navbar__list');
 const section_list=document.querySelectorAll('section');
-
+let i=1;
 // End Global Variables
 
-for(let i=0;i<section_list.length;i++)
-{
+section_list.forEach(element =>{
     // Build menu
     const list_item=document.createElement('li');
-    list_item.innerHTML=`<a href="#${section_list[i].id}" onClick="addMenuLinkClass(this,event)">`+'Section ' +(i+1)+`</a>`;
+    list_item.innerHTML=`<a href="#${element.id}" onClick="addMenuLinkClass(this,event)">`+'Section ' +i+`</a>`;
+    i++;
     nav_list.appendChild(list_item);
     list_item.addEventListener('click',function(event){
         event.preventDefault();
 
         // Scroll to section on link click
-        section_list[i].scrollIntoView({behavior: 'smooth'});
+        element.scrollIntoView({behavior: 'smooth'});
     });
-
-    var isInViewport = function (elem) {
-        var bounding = elem.getBoundingClientRect();
-        return (
-            bounding.top >= 0 &&
-            bounding.left >= 0 &&
-            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    };
 
     // Set sections as active
     window.addEventListener("scroll", function () {
 
         // Add class 'active' to section when near top of viewport
-        if (isInViewport(section_list[i])) {
-            section_list[i].classList.add("active-class");
+        if (isInViewport(element)) {
+            element.classList.add("active-class");
+            removeActiveClass(element);
         }
     });
+})
+
+const removeActiveClass=(el)=>{
+    section_list.forEach(sec=>{
+        if(sec!=el)
+        sec.classList.remove("active-class");
+    })
 }
 
+var isInViewport = (elem) => {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
 //Start Helper Functions
-function addMenuLinkClass(element,event){
+const addMenuLinkClass=(element,event)=>{
     element.classList.add("menu__link");
     const links=document.querySelectorAll("a");
-    console.log(element);
-    console.log(event.target);
     for(let i=0;i<links.length;i++)
     {
-        console.log(links[i]);
         if(element!=links[i])
             links[i].classList.remove("menu__link");
-
     }
 }

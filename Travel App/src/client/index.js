982967357173c8dcfea8b8a495d1e9e98852dca6
Gaurangi <manegaurangi89@gmail.com>
2020-04-18 18:30:59 +0200

    Delete project artifacts

--- Travel App/src/client/index.js
+++
@@ -1,7 +0,0 @@
-//import  {buttonEvent}  from './js/app'
-import app from './js/app'
-//import {urlChecker} from './js/urlChecker'
-import './styles/style.scss'
-import './styles/header.scss'
-
-export default app;
